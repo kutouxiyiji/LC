75a8b352031c91c62ad8da9a1320e1e61d202b06
kutouxiyiji <30358622+kutouxiyiji@users.noreply.github.com>
2018-10-30 22:08:09 -0700

    Add files via upload

---
+++ 86.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 22:07:00 2018
+
+@author: yongw
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def partition(self, head, x):
+        h1 = l1 = ListNode(0)
+        h2 = l2 = ListNode(0)
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+        l2.next = None
+        l1.next = h2.next
+        return h1.next
+            
+            
+            
+            
