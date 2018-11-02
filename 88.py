991d31f23201e084452cdfd2c403d972cca15f9a
kutouxiyiji <30358622+kutouxiyiji@users.noreply.github.com>
2018-11-01 19:47:25 -0700

    LC

---
+++ 88.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        if len(nums2)!=0:
+            nums1[-len(nums2):] = nums2
+        nums1.sort()
+        
